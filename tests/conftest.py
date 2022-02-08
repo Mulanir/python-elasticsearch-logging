885684fea289d464a33d8422b1f655032c3e3169
Bogdan Sydorov <bodiasyd@gmail.com>
2022-02-08 23:20:57 +0200

    Correcting Elasticsearch test container startup

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,21 +5,23 @@ import socket
 
 import pytest
 import docker
+import elasticsearch as es
+from elasticsearch.client import Elasticsearch
 
 
 @pytest.fixture(scope='session')
 def elastic_host():
-    sock = socket.socket()
-    sock.bind(('', 0))
-    port = sock.getsockname()[1]
+    port = _get_free_port()
 
     docker_client = docker.from_env()
-    docker_image_name = 'docker.elastic.co/elasticsearch/elasticsearch:7.17.0'
+    docker_image_name = 'elasticsearch:7.17.0'
     docker_container_name = 'elasticsearch_test_emulator'
 
     container = docker_client.containers.run(
         docker_image_name, detach=True, ports={
             '9200': port
+        }, environment={
+            'discovery.type': 'single-node'
         }, name=docker_container_name)
 
     for _ in range(60):
@@ -36,10 +38,35 @@ def elastic_host():
 
         raise TimeoutError('Elasticsearch container startup failed.')
 
-    yield f'http://localhost:{port}'
+    host = f'http://localhost:{port}'
+
+    for _ in range(120):
+        if _check_ready(host):
+            break
+        else:
+            time.sleep(1)
+    else:
+        try:
+            container.stop()
+            container.remove()
+        except Exception as ex:
+            logging.exception(ex)
+
+        raise TimeoutError('Elasticsearch container ready failed.')
+
+    yield host
 
     container.stop()
     container.remove()
+
+
+def _get_free_port():
+    sock = socket.socket()
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+
+    return port
 
 
 def _check_running(docker_client, container_name):
@@ -50,3 +77,25 @@ def _check_running(docker_client, container_name):
     container_is_running = container_state['Status'] == RUNNING
 
     return container_is_running
+
+
+def _check_ready(host):
+    try:
+        es_client = es.Elasticsearch(
+            hosts=[host])
+        es_client.info()
+
+        return True
+    except:
+        return False
+
+
+@pytest.fixture
+def get_logger():
+    def inner(handler):
+        test_logger = logging.getLogger('test')
+        test_logger.addHandler(handler)
+
+        return test_logger
+
+    return inner
